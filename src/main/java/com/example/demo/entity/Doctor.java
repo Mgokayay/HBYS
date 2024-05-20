7f4932478ba9fc1656aa43171508a33bc90c2bdf
Mgokayay <aymustafagokay@gmail.com>
2024-05-20 21:17:13 +0300

    doctor entity created

---
+++ src/main/java/com/example/demo/entity/Doctor.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "doctor",schema = "bizmed")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "department_id",nullable = false)
+    private Department department;
+
+}
