f719756799383c2df9eea91f8adaa70010bd159e
Mgokayay <aymustafagokay@gmail.com>
2024-05-20 21:17:34 +0300

    doctor dto convertion created

---
+++ src/main/java/com/example/demo/util/DoctorDtoConvertion.java
@@ -0,0 +1,25 @@
+package com.example.demo.util;
+
+import com.example.demo.dto.DoctorResponse;
+import com.example.demo.entity.Doctor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoctorDtoConvertion {
+
+
+    public static List<DoctorResponse> convertDoctorList(List<Doctor> doctors){
+        List<DoctorResponse> doctorResponses=new ArrayList<>();
+
+        doctors.stream().forEach(doctor -> doctorResponses.add(new DoctorResponse(doctor.getId(),
+                doctor.getName(),doctor.getDepartment().getId())));
+
+        return doctorResponses;
+    }
+
+    public static DoctorResponse convertDoctor(Doctor doctor){
+        return new DoctorResponse(doctor.getId(),
+                doctor.getName(),doctor.getDepartment().getId());
+    }
+}
