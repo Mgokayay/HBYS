05b4f3c4cb57144cb8144c83572bb755c713293e
Mgokayay <aymustafagokay@gmail.com>
2024-05-20 21:18:11 +0300

    doctor service implementation created

---
+++ src/main/java/com/example/demo/service/DoctorServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.DoctorResponse;
+import com.example.demo.entity.Doctor;
+import com.example.demo.exceptions.CommonException;
+import com.example.demo.repository.DoctorRepository;
+import com.example.demo.util.DoctorDtoConvertion;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorServiceImpl implements DoctorService{
+
+    private final DoctorRepository doctorRepository;
+
+    public DoctorServiceImpl(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    @Override
+    public DoctorResponse save(Doctor doctor) {
+        doctorRepository.save(doctor);
+        return DoctorDtoConvertion.convertDoctor(doctor);
+    }
+
+    @Override
+    public List<DoctorResponse> findAll() {
+        return DoctorDtoConvertion.convertDoctorList(doctorRepository.findAll());
+    }
+
+    @Override
+    public DoctorResponse findById(Long id) {
+        Optional<Doctor> doctorOptional = doctorRepository.findById(id);
+        if(doctorOptional.isPresent()){
+            return DoctorDtoConvertion.convertDoctor(doctorOptional.get());
+        }
+        throw new CommonException("Doctor not found with given id"+id,HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public DoctorResponse delete(Long id) {
+        Optional<Doctor> doctorOptional=doctorRepository.findById(id);
+        if(doctorOptional.isPresent()){
+            Doctor doctor = doctorOptional.get();
+            doctorRepository.delete(doctor);
+            return DoctorDtoConvertion.convertDoctor(doctor);
+
+        }
+        throw new CommonException("Doctor not found with given id"+id, HttpStatus.NOT_FOUND);
+    }
+}
