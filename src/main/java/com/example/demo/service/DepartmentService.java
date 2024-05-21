5234870478443011297b64c334debae24d1e6140
Mgokayay <aymustafagokay@gmail.com>
2024-05-21 19:35:28 +0300

    department service created

---
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.DepartmentResponse;
+import com.example.demo.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+
+    DepartmentResponse save(Department department);
+
+    List<DepartmentResponse> findAll();
+
+    void delete(Long id);
+
+    DepartmentResponse findById(Long id);
+}
