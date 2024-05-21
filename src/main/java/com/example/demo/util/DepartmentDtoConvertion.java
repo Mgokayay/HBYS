60c068427579396152c0d39fbe18c6ffab1f103c
Mgokayay <aymustafagokay@gmail.com>
2024-05-21 19:34:34 +0300

    department dto conversion created

---
+++ src/main/java/com/example/demo/util/DepartmentDtoConvertion.java
@@ -0,0 +1,22 @@
+package com.example.demo.util;
+
+import com.example.demo.dto.DepartmentResponse;
+import com.example.demo.entity.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentDtoConvertion {
+
+    public static List<DepartmentResponse> convertDepartmentList(List<Department> departments){
+        List<DepartmentResponse> departmentResponses = new ArrayList<>();
+        departments.stream().forEach(department -> departmentResponses.add(new DepartmentResponse(department.getId(),
+                department.getName())));
+
+        return departmentResponses;
+    }
+
+    public static DepartmentResponse convertDepartment(Department department){
+        return new DepartmentResponse(department.getId(),department.getName());
+    }
+}
