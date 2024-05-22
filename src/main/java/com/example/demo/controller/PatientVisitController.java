67ab4c4d8cc0eb50ab01f93d4b24be83ffe44b0c
Mgokayay <aymustafagokay@gmail.com>
2024-05-22 11:08:18 +0300

    patient visit controller created

---
+++ src/main/java/com/example/demo/controller/PatientVisitController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.PatientVisitResponse;
+import com.example.demo.entity.PatientVisit;
+import com.example.demo.service.PatientVisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("patientvisit")
+public class PatientVisitController {
+
+    private final PatientVisitService patientVisitService;
+
+    @Autowired
+    public PatientVisitController(PatientVisitService patientVisitService) {
+        this.patientVisitService = patientVisitService;
+    }
+
+    @PostMapping
+    public PatientVisitResponse save(@RequestBody PatientVisit patientVisit){
+        return patientVisitService.save(patientVisit);
+    }
+
+    @GetMapping
+    public List<PatientVisitResponse> findAll(){
+        return patientVisitService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PatientVisitResponse findById(@PathVariable Long id){
+        return patientVisitService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        patientVisitService.delete(id);
+    }
+}
