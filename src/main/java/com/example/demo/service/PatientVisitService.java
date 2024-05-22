c570f5180e9163804741c0f9055bb3511080d4df
Mgokayay <aymustafagokay@gmail.com>
2024-05-22 11:08:58 +0300

    patient visit service created

---
+++ src/main/java/com/example/demo/service/PatientVisitService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.PatientVisitResponse;
+import com.example.demo.entity.PatientVisit;
+
+import java.util.List;
+
+public interface PatientVisitService {
+
+    PatientVisitResponse save(PatientVisit patientVisit);
+
+    List<PatientVisitResponse> findAll();
+
+    PatientVisitResponse findById(Long id);
+
+    void delete(Long id);
+}
