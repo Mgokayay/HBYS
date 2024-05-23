b20dcaa99e33f0fdb1bf85b0505916b11ee96d30
Mgokayay <aymustafagokay@gmail.com>
2024-05-23 11:12:33 +0300

    department dto conversion edited

--- src/main/java/com/example/demo/util/DepartmentDtoConvertion.java
+++ src/main/java/com/example/demo/util/DepartmentDtoConvertion.java
@@ -11,12 +11,15 @@ public class DepartmentDtoConvertion {
     public static List<DepartmentResponse> convertDepartmentList(List<Department> departments){
         List<DepartmentResponse> departmentResponses = new ArrayList<>();
         departments.stream().forEach(department -> departmentResponses.add(new DepartmentResponse(department.getId(),
-                department.getName())));
+                department.getName(),DoctorDtoConvertion.convertDoctorList(department.getDoctors()
+        ), PatientVisitDtoConvertion.convertPatientVisitList(department.getPatientVisits()))));
 
         return departmentResponses;
     }
 
     public static DepartmentResponse convertDepartment(Department department){
-        return new DepartmentResponse(department.getId(),department.getName());
+        return new DepartmentResponse(department.getId(),department.getName()
+                ,DoctorDtoConvertion.convertDoctorList(department.getDoctors()
+        ), PatientVisitDtoConvertion.convertPatientVisitList(department.getPatientVisits()));
     }
 }
