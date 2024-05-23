30fe8bded0201b9da908ae26c9087761ebd18444
Mgokayay <aymustafagokay@gmail.com>
2024-05-23 11:16:12 +0300

    a new method added to the doctor service implementation

--- src/main/java/com/example/demo/service/DoctorServiceImpl.java
+++ src/main/java/com/example/demo/service/DoctorServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.DoctorResponse;
+import com.example.demo.entity.Department;
 import com.example.demo.entity.Doctor;
 import com.example.demo.exceptions.CommonException;
 import com.example.demo.repository.DoctorRepository;
 import com.example.demo.util.DoctorDtoConvertion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +17,21 @@ import java.util.Optional;
 public class DoctorServiceImpl implements DoctorService{
 
     private final DoctorRepository doctorRepository;
+    private final DepartmentService departmentService;
 
-    public DoctorServiceImpl(DoctorRepository doctorRepository) {
+    @Autowired
+    public DoctorServiceImpl(DoctorRepository doctorRepository, DepartmentService departmentService) {
         this.doctorRepository = doctorRepository;
+        this.departmentService = departmentService;
     }
 
     @Override
-    public DoctorResponse save(Doctor doctor) {
+    public DoctorResponse save(Doctor doctor,Long departmentId) {
+        Department department=departmentService.findByDepartmentId(departmentId);
+        doctor.setDepartment(department);
+        department.getDoctors().add(doctor);
+
+
         doctorRepository.save(doctor);
         return DoctorDtoConvertion.convertDoctor(doctor);
     }
@@ -50,5 +60,14 @@ public class DoctorServiceImpl implements DoctorService{
 
         }
         throw new CommonException("Doctor not found with given id"+id, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public Doctor findByDoctorId(Long doctorId) {
+        Optional<Doctor> doctorOptional = doctorRepository.findById(doctorId);
+        if(doctorOptional.isPresent()){
+            return doctorOptional.get();
+        }
+        throw new CommonException("Doctor not found with given id"+doctorId,HttpStatus.NOT_FOUND);
     }
 }
