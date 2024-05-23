31fb996b96ccbcbc84bff731042e1c89892a35d6
Mgokayay <aymustafagokay@gmail.com>
2024-05-23 11:11:49 +0300

    some little changes have been done

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("department")
+@RequestMapping("/department")
 public class DepartmentController {
 
     private final DepartmentService departmentService;
