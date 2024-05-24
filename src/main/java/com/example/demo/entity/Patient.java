e84426cde97b442b574a678dcf063ba646e6e351
Mgokayay <aymustafagokay@gmail.com>
2024-05-24 15:45:57 +0300

    jsonbackreference and validtckn annotations added to the patient entity

--- src/main/java/com/example/demo/entity/Patient.java
+++ src/main/java/com/example/demo/entity/Patient.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
+import com.example.demo.validation.ValidTckn;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,9 +32,12 @@ public class Patient {
     @Column(name = "gender",nullable = false)
     private Gender gender;
 
+    @NotNull
+    @ValidTckn
     @Column(name = "tckn",nullable = false,unique = true)
     private Long tckn;
 
-    @OneToMany(cascade = {CascadeType.ALL},mappedBy = "patientvisit")
+    @JsonBackReference
+    @OneToMany(cascade = {CascadeType.ALL},mappedBy = "patient")
     private List<PatientVisit> patientVisits=new ArrayList<>();
 }
