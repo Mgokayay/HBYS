789d70164a1f9a94e873187bb57f5ec5e7259be7
Mgokayay <aymustafagokay@gmail.com>
2024-05-24 15:46:59 +0300

    delete method's return value changed

--- src/main/java/com/example/demo/controller/PatientController.java
+++ src/main/java/com/example/demo/controller/PatientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("patient")
+@RequestMapping("/patient")
 public class PatientController {
 
     private final PatientService patientService;
@@ -35,7 +35,8 @@ public class PatientController {
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
-        patientService.delete(id);
+    public String delete(@PathVariable Long id){
+        return patientService.delete(id);
+
     }
 }
