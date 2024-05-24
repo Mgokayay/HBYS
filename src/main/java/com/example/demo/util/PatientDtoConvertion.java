a5fd566e7266c6867f9ec4f2e8da0afd0f27d401
Mgokayay <aymustafagokay@gmail.com>
2024-05-24 15:47:50 +0300

    new return value added to the patient dto conversion

--- src/main/java/com/example/demo/util/PatientDtoConvertion.java
+++ src/main/java/com/example/demo/util/PatientDtoConvertion.java
@@ -11,11 +11,13 @@ public class PatientDtoConvertion {
     public static List<PatientResponse> convertPatientList(List<Patient> patients){
         List<PatientResponse> patientResponses=new ArrayList<>();
         patients.stream().forEach(patient -> patientResponses.add(new PatientResponse(patient.getId(),patient.getFullName(),
-                patient.getGender().toString(),patient.getTckn())));
+                patient.getGender().toString(),patient.getTckn()
+        ,PatientVisitDtoConvertion.convertPatientVisitList(patient.getPatientVisits()))));
         return patientResponses;
     }
 
     public static PatientResponse convertPatient(Patient patient){
-        return new PatientResponse(patient.getId(), patient.getFullName(), patient.getGender().toString(),patient.getTckn());
+        return new PatientResponse(patient.getId(), patient.getFullName(), patient.getGender().toString(),patient.getTckn()
+        ,PatientVisitDtoConvertion.convertPatientVisitList(patient.getPatientVisits()));
     }
 }
