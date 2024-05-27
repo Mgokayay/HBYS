3c2c3a3ebf00f0a8245d27c07d888c40636f2f48
Mgokayay <aymustafagokay@gmail.com>
2024-05-27 22:10:14 +0300

    patient visit service edited

--- src/main/java/com/example/demo/service/PatientVisitService.java
+++ src/main/java/com/example/demo/service/PatientVisitService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface PatientVisitService {
 
     PatientVisitResponse save(PatientVisit patientVisit
-    ,Long departmentId,Long doctorId,Long patientId);
+    );
 
     List<PatientVisitResponse> findAll();
 
